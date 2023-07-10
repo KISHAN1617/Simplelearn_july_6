b0d16a835d4b67e8083b075c265ff6a736206da2
User <User@DESKTOP-CCUD8MA>
2023-07-10 13:01:58 +0530

    new

---
+++ SeleniumBasics/src/Demo/BrowserProfiling.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BrowserProfiling {
+	public static void main(String[] args) {
+		ChromeOptions options= new ChromeOptions();
+		options.addArguments("--disable -notifications");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.yatra.com");
+	}
+
+}
